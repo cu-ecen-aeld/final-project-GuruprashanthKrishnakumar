2a5f28095f0631ea59e1d45ec370ad3de97a95ae
Jordi <jordi.cros.mompart@gmail.com>
2022-11-22 05:53:22 +0000

    Newer UART

--- uart_driver/uart_driver.c
+++ uart_driver/uart_driver.c
@@ -1,5 +1,5 @@
 /*
-*  hw_serial.c - A "Hello World" serial UART driver for the Beaglebone Black
+*  uart_serial.c - A "Hello World" serial UART driver for the Beaglebone Black
 *
 *  Copyright (C) 2020, Alex Rhodes <https://www.alexrhodes.io>
 * 
@@ -33,11 +33,13 @@
 #include <linux/wait.h>
 #include <linux/interrupt.h>
 #include <linux/spinlock.h>
+#include <linux/slab.h>
+
 
 #define BUFF_SIZE 512
 
 //Circular buffer struct
-static struct circ_buff
+struct circ_buff
 {
     char buff[BUFF_SIZE];
     int read_pos;
@@ -46,7 +48,7 @@ static struct circ_buff
 };
 
 //Serial device struct
-static struct hw_serial_dev
+struct uart_serial_dev
 {
     void __iomem *regs;
     struct miscdevice mDev;
@@ -59,43 +61,50 @@ static struct hw_serial_dev
 };
 
 //Driver probe routine
-static int hw_probe(struct platform_device *pdev);
+static int uart_probe(struct platform_device *pdev);
 
 //Driver remove routine
-static int hw_remove(struct platform_device *pdev);
+static int uart_remove(struct platform_device *pdev);
 
 //FOPS open
-static int hw_open(struct inode *inode, struct file *file);
+static int uart_open(struct inode *inode, struct file *file);
 
 //FOPS close
-static int hw_close(struct inode *inodep, struct file *filp);
+static int uart_close(struct inode *inodep, struct file *filp);
 
 //FOPS read
-static ssize_t hw_read(struct file *file, char __user *buf, size_t size, loff_t *ppos);
+static ssize_t uart_read(struct file *file, char __user *buf, size_t size, loff_t *ppos);
 
 //FOPS write
-static ssize_t hw_write(struct file *file, const char __user *buf, size_t len, loff_t *ppos);
+static ssize_t uart_write(struct file *file, const char __user *buf, size_t len, loff_t *ppos);
+
+//UART receive from another LKM
+ssize_t uart_receive(char *buf, size_t size);
+EXPORT_SYMBOL(uart_receive);
+//UART send from another LKM
+ssize_t uart_send(const char *buf, size_t len);
+EXPORT_SYMBOL(uart_send);
 
 //Routine to read from serial device registers
-static unsigned int reg_read(struct hw_serial_dev *dev, int offset);
+static unsigned int reg_read(struct uart_serial_dev *dev, int offset);
 
 //Routine to write to serial device registers
-static void reg_write(struct hw_serial_dev *dev, int val, int offset);
+static void reg_write(struct uart_serial_dev *dev, int val, int offset);
 
 //Routine to write a character to the seriald device
-static void write_char(struct hw_serial_dev *dev, char test);
+static void write_char(struct uart_serial_dev *dev, char test);
 
 //Interrupt handler
 static irqreturn_t irqHandler(int irq, void *devid);
 
 //Utility method to write to circular buffer
-static void write_circ_buff(char c, struct hw_serial_dev *dev);
+static void write_circ_buff(char c, struct uart_serial_dev *dev);
 
 //Utility method to read from circular buff
-static char read_circ_buff(struct hw_serial_dev *dev);
+static char read_circ_buff(struct uart_serial_dev *dev);
 
 //Device id struct
-static struct of_device_id hw_match_table[] =
+static struct of_device_id uart_match_table[] =
     {
         {
             .compatible = "serial",
@@ -103,55 +112,71 @@ static struct of_device_id hw_match_table[] =
 };
 
 //File operations struct
-static const struct file_operations hw_fops = {
+static const struct file_operations uart_fops = {
     .owner = THIS_MODULE,
-    .read = hw_read,
-    .write = hw_write,
-    .open = hw_open,
-    .release = hw_close,
+    .read = uart_read,
+    .write = uart_write,
+    .open = uart_open,
+    .release = uart_close,
     .llseek = no_llseek,
 };
 
 //Platform driver structure
-static struct platform_driver hw_plat_driver = {
+static struct platform_driver uart_plat_driver = {
     .driver = {
         .name = "serial",
         .owner = THIS_MODULE,
-        .of_match_table = hw_match_table},
-    .probe = hw_probe,
-    .remove = hw_remove
+        .of_match_table = uart_match_table},
+    .probe = uart_probe,
+    .remove = uart_remove
 };
 
+//Driver instance
+struct uart_serial_dev *dev;
+
 /*********************************************************/
-static int hw_open(struct inode *inode, struct file *file)
+static int uart_open(struct inode *inode, struct file *file)
 {
     return 0;
 }
 /*********************************************************/
-static int hw_close(struct inode *inodep, struct file *filp)
+static int uart_close(struct inode *inodep, struct file *filp)
 {
     return 0;
 }
 /*********************************************************/
-static ssize_t hw_read(struct file *file, char __user *buf, size_t size, loff_t *ppos)
+static ssize_t uart_read(struct file *file, char __user *buf, size_t size, loff_t *ppos)
 {
     struct miscdevice *mdev = (struct miscdevice *)file->private_data;
-    struct hw_serial_dev *dev = container_of(mdev, struct hw_serial_dev, mDev);
+    struct uart_serial_dev *dev = container_of(mdev, struct uart_serial_dev, mDev);
+    char ret;
     wait_event_interruptible(dev->waitQ, dev->buf.length > 0);
 
-    char ret = read_circ_buff(dev);
-    copy_to_user(buf, &ret, 1);
+    ret = read_circ_buff(dev);
+    if(copy_to_user(buf, &ret, 1))
+    {
+        printk("uart: cannot copy memory to the user.\n");
+        return 0;
+    }
     return 1;
 }
 /*********************************************************/
-static ssize_t hw_write(struct file *file, const char __user *buf, size_t len, loff_t *ppos)
+static ssize_t uart_write(struct file *file, const char __user *buf, size_t len, loff_t *ppos)
 {
     struct miscdevice *mdev = (struct miscdevice *)file->private_data;
-    struct hw_serial_dev *dev = container_of(mdev, struct hw_serial_dev, mDev);
-
-    char kmem[len + 1];
-    copy_from_user(kmem, buf, len);
+    struct uart_serial_dev *dev = container_of(mdev, struct uart_serial_dev, mDev);
     int i;
+    char *kmem = kmalloc(sizeof(char)*(len + 1), GFP_KERNEL);
+    if(!kmem)
+    {
+        printk("uart: cannot allocate memory for a write operation.\n");
+        return 0;
+    }
+    if(copy_from_user(kmem, buf, len))
+    {
+        printk("uart: cannot copy memory from the user.\n");
+        return 0;
+    }
     for (i = 0; i < len; i++)
     {
         if (kmem[i] == '\n')
@@ -163,21 +188,70 @@ static ssize_t hw_write(struct file *file, const char __user *buf, size_t len, l
         {
             write_char(dev, kmem[i]);
         }
-    }   
+    }
+    kfree(kmem);   
     return len;
 }
 
+
 /*********************************************************/
-static unsigned int reg_read(struct hw_serial_dev *dev, int offset)
+ssize_t uart_receive(char *buf, size_t size)
 {
+    char ret;
+    wait_event_interruptible(dev->waitQ, dev->buf.length > 0);
+
+    //An interesting approach is to sleep until a expected number of bytes is received
+
+    ret = read_circ_buff(dev);
+    *buf = ret;
+
+    return 1;
+}
+
+/*********************************************************/
+ssize_t uart_send(const char *buf, size_t len)
+{
+    int i;
+    char *kmem = kmalloc(sizeof(char)*(len + 1), GFP_KERNEL);
+    if(!kmem)
+    {
+        printk("uart: cannot allocate memory for a write operation.\n");
+        return 0;
+    }
+    if(memcpy(kmem, buf, len))
+    {
+        printk("uart: cannot copy memory from the user.\n");
+        return 0;
+    }
+    for (i = 0; i < len; i++)
+    {
+        if (kmem[i] == '\n')
+        {
+            write_char(dev, '\n');
+            write_char(dev, '\r');
+        }
+        else
+        {
+            write_char(dev, kmem[i]);
+        }
+    }
+    kfree(kmem);   
+    return len;
+}
+
+
+/*********************************************************/
+static unsigned int reg_read(struct uart_serial_dev *dev, int offset)
+{
+    unsigned int ret;
     spin_lock_irqsave(&dev->lock, dev->irqFlags);
-    unsigned int ret = ioread32(dev->regs + (4 * offset));
+    ret = ioread32(dev->regs + (4 * offset));
     spin_unlock_irqrestore(&dev->lock, dev->irqFlags);
     return ret;
 }
 
 /*********************************************************/
-static void reg_write(struct hw_serial_dev *dev, int val, int offset)
+static void reg_write(struct uart_serial_dev *dev, int val, int offset)
 {
     spin_lock_irqsave(&dev->lock, dev->irqFlags);
     iowrite32(val, dev->regs + (4 * offset));
@@ -186,7 +260,7 @@ static void reg_write(struct hw_serial_dev *dev, int val, int offset)
 }
 
 /*********************************************************/
-static void write_char(struct hw_serial_dev *dev, char c)
+static void write_char(struct uart_serial_dev *dev, char c)
 {
     unsigned int lsr = reg_read(dev, UART_LSR);
     while (1)
@@ -203,7 +277,7 @@ static void write_char(struct hw_serial_dev *dev, char c)
 /*********************************************************/
 static irqreturn_t irqHandler(int irq, void *d)
 {
-    struct hw_serial_dev *dev = d;
+    struct uart_serial_dev *dev = d;
     do 
     {
         char recv = reg_read(dev, UART_RX);
@@ -215,7 +289,7 @@ static irqreturn_t irqHandler(int irq, void *d)
 }
 
 /*********************************************************/
-static void write_circ_buff(char c, struct hw_serial_dev *dev)
+static void write_circ_buff(char c, struct uart_serial_dev *dev)
 {
     spin_lock_irqsave(&dev->lock, dev->irqFlags);
     if(dev->buf.length < BUFF_SIZE)
@@ -228,10 +302,11 @@ static void write_circ_buff(char c, struct hw_serial_dev *dev)
 }
 
 /*********************************************************/
-static char read_circ_buff(struct hw_serial_dev *dev)
+static char read_circ_buff(struct uart_serial_dev *dev)
 {
+    char c;
     spin_lock_irqsave(&dev->lock, dev->irqFlags);
-    char c = dev->buf.buff[dev->buf.read_pos];
+    c = dev->buf.buff[dev->buf.read_pos];
     dev->buf.buff[dev-> buf.read_pos] = '\0';
     if(dev->buf.length > 0)
     {
@@ -245,9 +320,13 @@ static char read_circ_buff(struct hw_serial_dev *dev)
 
 
 /*********************************************************/
-static int hw_probe(struct platform_device *pdev)
+static int uart_probe(struct platform_device *pdev)
 {
     struct resource *res;
+    int ret, error;
+    //Configure the UART device
+    unsigned int baud_divisor;
+    unsigned int uartclk;
     res = platform_get_resource(pdev, IORESOURCE_MEM, 0);
 
     if (!res)
@@ -256,7 +335,7 @@ static int hw_probe(struct platform_device *pdev)
         return -EINVAL;
     }
 
-    struct hw_serial_dev *dev = devm_kzalloc(&pdev->dev, sizeof(struct hw_serial_dev), GFP_KERNEL);
+    dev = devm_kzalloc(&pdev->dev, sizeof(struct uart_serial_dev), GFP_KERNEL);
     if (!dev)
     {
         pr_err("%s: devm_kzalloc returned NULL\n", __func__);
@@ -276,7 +355,7 @@ static int hw_probe(struct platform_device *pdev)
 		dev_err(&pdev->dev, "%s: unable to get IRQ\n", __func__);
 		return dev->irq;
 	}
-	int ret = devm_request_irq(&pdev->dev, dev->irq, irqHandler, 0, "hw_serial", dev);
+	ret = devm_request_irq(&pdev->dev, dev->irq, irqHandler, 0, "uart_serial", dev);
 	if (ret < 0) 
     {
 		dev_err(&pdev->dev, "%s: unable to request IRQ %d (%d)\n", __func__, dev->irq, ret);
@@ -291,10 +370,6 @@ static int hw_probe(struct platform_device *pdev)
     //Enable power management runtime
     pm_runtime_enable(&pdev->dev);
     pm_runtime_get_sync(&pdev->dev);
-
-    //Configure the UART device
-    unsigned int baud_divisor;
-    unsigned int uartclk;
 
     of_property_read_u32(pdev->dev.of_node, "clock-frequency", &uartclk);
 
@@ -312,10 +387,10 @@ static int hw_probe(struct platform_device *pdev)
 
     //Initialize and register a misc device
     dev->mDev.minor = MISC_DYNAMIC_MINOR;
-    dev->mDev.name = devm_kasprintf(&pdev->dev, GFP_KERNEL, "hw_serial-%x", res->start);
-    dev->mDev.fops = &hw_fops;
+    dev->mDev.name = devm_kasprintf(&pdev->dev, GFP_KERNEL, "uart_serial-%x", res->start);
+    dev->mDev.fops = &uart_fops;
 
-    int error = misc_register(&dev->mDev);
+    error = misc_register(&dev->mDev);
     if (error)
     {
         pr_err("%s: misc register failed.", __func__);
@@ -331,10 +406,11 @@ static int hw_probe(struct platform_device *pdev)
 }
 
 /*********************************************************/
-static int hw_remove(struct platform_device *pdev)
+static int uart_remove(struct platform_device *pdev)
 {
+    struct uart_serial_dev *dev;
     pm_runtime_disable(&pdev->dev);
-    struct hw_serial_dev *dev = dev_get_drvdata(&pdev->dev);
+    dev = dev_get_drvdata(&pdev->dev);
     misc_deregister(&dev->mDev);
     return 0;
 }
@@ -343,4 +419,4 @@ MODULE_AUTHOR("Alex Rhodes");
 MODULE_LICENSE("GPL");
 
 //Register the platform driver
-module_platform_driver(hw_plat_driver);
+module_platform_driver(uart_plat_driver);
